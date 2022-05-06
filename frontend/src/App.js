137e69955f7fd09bc7fa8b360908198c3278f46c
Sambhav <surana.sambhav@antiersolutions.com>
2022-05-06 17:39:32 +0530

    Added Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/common/Navbar";
+import Home from "./Components/Home";
+import Add from "./Components/Add";
+import View from "./Components/View";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/add" element={<Add />} />
+          <Route exact path="/view" element={<View />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/View/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const View = () => {
+  return (
+    <div className='container mt-5'>
+        <h1>View</h1>
+    </div>
+    
+  )
+}
+
+export default View
